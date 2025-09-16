8b4a17e25b259051a3c9cc38feef832a683149d2
PrabhasKS <ksprabhas.08@gmail.com>
2025-09-16 15:34:51 +0530

    introduced hash algorithm to add initial admin

---
+++ server/hash_password.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcrypt');
+
+  // --- CHANGE THE PASSWORD IN THE LINE BELOW ---
+  const plainTextPassword = 'superadmin';
+  // -----------------------------------------
+
+  const saltRounds = 10;
+
+  bcrypt.hash(plainTextPassword, saltRounds, function(err, hash) {
+      if (err) {
+          console.error("Error hashing password:", err);
+          return;
+      }
+      console.log("Your hashed password is:");
+      console.log(hash);
+  });
